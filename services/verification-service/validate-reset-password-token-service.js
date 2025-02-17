8a09a6c97a62f7c0f4e484e83436d9187e156c39
Aditya Goyal <aditya.1155@zenmonk.tech>
2025-02-17 18:28:49 +0530

    Feature: Fixed validate_reset_password_service

--- services/verification-service/validate-reset-password-token-service.js
+++ services/verification-service/validate-reset-password-token-service.js
@@ -1,14 +1,22 @@
 const { BadRequest } = require("../../libs/error");
 const VerificationService = require("./verification-service");
-const { userStatus } = require("../../models/user/user-status");
 const { User } = require("../../models");
 
 module.exports = class ValidateResetPasswordTokenService extends VerificationService {
   _execute = async ({ token, transaction }) => {
-    const verificationLog = await this.verification_logs_repository.findOne({
+    
+    let verificationLog = await this.verification_logs_repository.findOne({
       criteria: { uuid: token },
-      options: { transaction },
-      include: [{ model: User, as: "user_details" }],
+      options: {
+        transaction,
+        plain: true
+      },
+      attributes: ["uuid", "expires_at"],
+      include: [{
+        model: User,
+        as: "user_details",
+        attributes: ["email", "name"]
+      }],
     });
 
     if (!verificationLog) throw new BadRequest("Token Invalid!");
