72a7dad9fa995be749034938ddf6068cc42a98ee
Aditya Goyal <aditya.1155@zenmonk.tech>
2025-02-17 18:10:47 +0530

    Feature: Fixed forgot_password_service

--- services/verification-service/send-reset-password-token-service.js
+++ services/verification-service/send-reset-password-token-service.js
@@ -1,7 +1,14 @@
 const { InternalServerError } = require("../../libs/error");
+const SendVerificationTokenEvent = require("../../models/verification/event/send-verification-token-event");
 const VerificationService = require("./verification-service");
 
 module.exports = class SendResetPasswordTokenService extends VerificationService {
+
+  constructor({ verification_logs_repository, outbox_message_repository }) {
+    super({ verification_logs_repository });
+    this.outbox_message_repository = outbox_message_repository;
+  }
+  
   _execute = async ({ purpose = "reset_password", user, transaction }) => {
     
     const verificationLog = await this.verification_logs_repository.create({
@@ -14,17 +21,25 @@ module.exports = class SendResetPasswordTokenService extends VerificationService
       },
       options: { transaction },
     });
-
-    // const token = verificationLog.uuid;
-    // const client = process.env.CLIENT_URL;
-
-    // await mail_service.mail_reset_link({
-    //   to: user.email,
-    //   subject: "Reset Password Link",
-    //   url: `${client}/reset_password/${token}`,
-    //   name: user.name,
-    // });                                      // EDA coming soon
-
+    
     if (!verificationLog) throw new InternalServerError("Error in sending Reset Password Link");
+
+    const token = verificationLog.uuid;
+    const client = process.env.CLIENT_URL;
+
+    const mailData = {
+      to: user.email,
+      subject: "Reset Password Link",
+      url: `${client}/reset_password/${token}`,
+      name: user.name,
+    };  
+    
+    await this.outbox_message_repository.store_outbox_message({
+      outbox_message: new SendVerificationTokenEvent(mailData),
+      options: { transaction },
+    })
+
+    return
+
   };
 };
